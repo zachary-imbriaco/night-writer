af0d10b2e5477c3746f91b6f161831f535530a54
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-04 16:38:50 -0500

    Create test for reading message file.

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -2,7 +2,13 @@ require './lib/night_writer'
 
 RSpec.describe 'Night Writer' do
   night_writer = NightWriter.new('./data/test_data.txt')
-  it 'Night Writer returns a message to CLI' do
+
+  it 'outputs a message to CLI' do
     expect(night_writer.output).not_to eq ''
+  end
+
+  it 'reads the provided message file' do
+    expect(night_writer.read_message).to eq 'the quick brown fox jumps over the lazy dog'
+
   end
 end
