d9705891257e3da7525bce8096c0964632ddc4ae
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-07 15:14:26 -0500

    reader_spec added to spec_helper.

--- lib/reader.rb
+++ lib/reader.rb
@@ -1,11 +1,11 @@
 require './lib/dictionary'
 
 class Reader
-  attr_reader :file_name :output_name :output_file
+  attr_reader :file_name, :output_name, :output_file
 
-  def initialize(file_name)
+  def initialize(file_name, output_name)
     @file_name = file_name
     @output_name = output_name
-    @output_file = File.new("./data/#{output_name}", "w")
+    @output_file = File.open("./data/#{output_name}", "w")
   end
 end
