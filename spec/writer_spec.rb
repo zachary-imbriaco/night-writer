5e14f33fcda29c4bef2f42da09b07534e42afff9
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-07 12:36:40 -0500

    Create single letter test writer in spec.

--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -2,17 +2,20 @@ require './lib/writer'
 
 RSpec.describe 'Writer' do
   ARGV = ["test_data.txt", "braille.txt"]
-  night_writer = Writer.new(ARGV[0], ARGV[1])
+  night_writer1 = Writer.new(ARGV[0], ARGV[1])
+  ARGV = ["single_letter_test.txt", "single_letter_output.txt"]
+  night_writer2 = Writer.new(ARGV[0], ARGV[1])
+
 
   it 'outputs a message to CLI' do
-    expect(night_writer.output).not_to eq ''
+    expect(night_writer1.output).not_to eq ''
   end
 
   it 'reads the provided message file' do
-    expect(night_writer.read_message).to eq 'the quick brown fox jumps over the lazy dog'
+    expect(night_writer1.read_message).to eq 'the quick brown fox jumps over the lazy dog'
   end
 
   it 'counts the characters in the provided message file' do
-    expect(night_writer.char_count).to eq(43)
+    expect(night_writer1.char_count).to eq(43)
   end
 end
