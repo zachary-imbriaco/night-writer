16305771f8c57c6002410b20aad4fa128fa9dabf
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-07 12:32:25 -0500

    Fix incorrect method calls in dictionary_spec.

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe 'Dictionary' do
   writer = Writer.new("test_data.txt", "braille.txt")
 
   it 'contains a dictionary of lowercase braille letters and space' do
-    expect(writer.dictionary.length).to eq 27
+    expect(writer.braille_dict.length).to eq 27
   end
 
   it 'contains an array of possible braille outputs' do
-    expect(writer.braille[3]).to eq "00"
+    expect(writer.braille_outs[3]).to eq "00"
   end
 end
